ddb51e2637180e40143b8cfe8a49ca448be335af
Joe-Hsn <youssef.hassan.mahmoud@gmail.com>
2022-11-19 16:22:10 +0200

    navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+type Props = {};
+
+const links: string[] = ["Home", "About", "Contact", "Blog", "Careers"];
+
+export default function Navbar({}: Props) {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav>
+      <Image src="/logo.svg" alt="Logo" width={140} height={20} priority />
+      <ul className={!toggle ? "hidden lg:flex" : "block lg:flex"}>
+        {links.map((link, index) => (
+          <li key={index}>
+            <Link href={link.toLowerCase()}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+      <a href="#request-invite" className="btn">
+        Request Invite
+      </a>
+      <button
+        className="nav__toggler"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        {!toggle ? (
+          <Image
+            width={24}
+            height={11}
+            src="/icon-hamburger.svg"
+            alt="menu toggler's icon"
+          />
+        ) : (
+          <Image
+            width={18}
+            height={19}
+            src="/icon-close.svg"
+            alt="menu close's icon"
+          />
+        )}
+      </button>
+    </nav>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // ### Primary
+        "dark-blue": "hsl(233, 26%, 24%)",
+        "lime-green": "hsl(136, 65%, 51%)",
+        "bright-cyan": "hsl(192, 70%, 51%)",
+        // ### Neutral
+        "grayish-blue": "hsl(233, 8%, 62%)",
+        "light-grayish-blue": "hsl(220, 16%, 96%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        "public-sans": ["Public Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
