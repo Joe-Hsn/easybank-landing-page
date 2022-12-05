d969af6ef5f8ffc3f32e0f560c92835b79e228d7
Joe-Hsn <youssef.hassan.mahmoud@gmail.com>
2022-12-05 13:15:55 +0200

    dark mode last

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,10 +9,14 @@ export default function Hero({}: Props) {
     <header className="hero">
       <div className="hero__container">
         <div className="hero__content">
-          <h1>Next generation digital banking</h1>
-          Take your financial life online. Your Easybank account will be a
-          one-stop-shop for spending, saving, budgeting, investing, and much
-          more.
+          <h1 className="text-4xl lg:text-6xl">
+            Next generation digital banking
+          </h1>
+          <div className="text-sm lg:text-lg text-grayish-blue">
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </div>
           <Link href="#request-invite" className="btn">
             Request Invite
           </Link>
